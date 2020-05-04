cd4da0ecff61d20fa28cd127d2c0302ca432dd00
ekuinox <depkey@me.com>
2020-05-05 03:47:09 +0900

    oauth2でGitHubから飛んで帰ってくるまで

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,63 +1,23 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
 extern crate base64;
 extern crate oauth2;
 extern crate rand;
-extern crate rocket;
 extern crate rocket_contrib;
+#[macro_use] extern crate rocket;
 
-use oauth2::{
-    AuthorizationCode,
-    AuthUrl,
-    ClientId,
-    ClientSecret,
-    CsrfToken,
-    RedirectUrl,
-    Scope,
-    TokenResponse,
-    TokenUrl,
-    PkceCodeChallenge
-};
-use oauth2::basic::BasicClient;
-use dotenv::dotenv;
-use oauth2::reqwest::http_client;
-use std::env;
+mod routes;
+
 use rocket_contrib::serve::StaticFiles;
+use dotenv::dotenv;
 
 fn main() {
-    rocket::ignite()
-        .mount("/", StaticFiles::from(concat!(env!("CARGO_MANIFEST_DIR"), "/../frontend/dist")))
-        .launch();
-
-    /*
     dotenv().ok();
 
-    let client_id = ClientId::new(env::var("RED_DRINK_GITHUB_CLIENT_ID").expect("missing RED_DRINK_GITHUB_CLIENT_ID"));
-    let client_secret = ClientSecret::new(env::var("RED_DRINK_GITHUB_CLIENT_SECRET").expect("missing RED_DRINK_GITHUB_CLIENT_SECRET"));
-    let auth_url = AuthUrl::new("https://github.com/login/oauth/authorize".to_string()).unwrap();
-    let token_url = TokenUrl::new("https://github.com/login/oauth/access_token".to_string()).unwrap();
+    use routes::*;
 
-    let client = BasicClient::new(
-        client_id,
-        Some(client_secret),
-        auth_url,
-        Some(token_url)
-    ).set_redirect_url(RedirectUrl::new("http://localhost:8080".to_string()).unwrap());
-
-    let (pkce_challenge, pkce_verifier) = PkceCodeChallenge::new_random_sha256();
-    
-    let (authorize_url, csrf_secret) = client
-        .authorize_url(CsrfToken::new_random)
-        .add_scope(Scope::new("public_repo".to_string()))
-        .add_scope(Scope::new("user:email".to_string()))
-        .set_pkce_challenge(pkce_challenge)
-        .url();
-    
-    println!("{}", authorize_url);
-
-    let token_result = client
-        .exchange_code(AuthorizationCode::new("some authorization code".to_string()))
-        .set_pkce_verifier(pkce_verifier)
-        .request(http_client).unwrap();
-
-    println!("{}", token_result.access_token().secret());
-    */
+    rocket::ignite()
+        .mount("/", routes![get_token, auth])
+        .mount("/", StaticFiles::from(concat!(env!("CARGO_MANIFEST_DIR"), "/../frontend/dist")))
+        .launch();
 }
---
+++ backend/src/routes.rs
@@ -0,0 +1,49 @@
+use rocket::response::Redirect;
+use oauth2::{
+    AccessToken,
+    AuthUrl,
+    ClientId,
+    ClientSecret,
+    CsrfToken,
+    PkceCodeChallenge,
+    RedirectUrl,
+    Scope,
+    TokenUrl,
+    TokenResponse,
+    AuthorizationCode
+};
+use oauth2::basic::BasicClient;
+use oauth2::reqwest::http_client;
+use std::env;
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref Client: BasicClient = BasicClient::new(
+        ClientId::new(env::var("RED_DRINK_GITHUB_CLIENT_ID").expect("missing RED_DRINK_GITHUB_CLIENT_ID")),
+        Some(ClientSecret::new(env::var("RED_DRINK_GITHUB_CLIENT_SECRET").expect("missing RED_DRINK_GITHUB_CLIENT_SECRET"))),
+        AuthUrl::new("https://github.com/login/oauth/authorize".to_string()).unwrap(),
+        Some(TokenUrl::new("https://github.com/login/oauth/access_token".to_string()).unwrap())
+    ).set_redirect_url(RedirectUrl::new(env::var("RED_DRINK_GITHUB_REDIRECT_URL").unwrap().to_string()).unwrap());
+}
+
+#[get("/get_token")]
+pub fn get_token() -> Redirect {
+    let (authorize_url, csrf_secret) = Client
+        .authorize_url(CsrfToken::new_random)
+        .add_scope(Scope::new("public_repo".to_string()))
+        .add_scope(Scope::new("user:email".to_string()))
+        .url();
+
+
+    Redirect::to(authorize_url.into_string())
+}
+
+#[get("/auth?<code>&<state>")]
+pub fn auth(code: String, state: String) -> Redirect {
+    let token_result = Client
+        .exchange_code(AuthorizationCode::new(code))
+        // Set the PKCE code verifier.
+        .request(http_client).unwrap();
+
+    Redirect::to(format!("/?token={}", token_result.access_token().secret()))
+}
