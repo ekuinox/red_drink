69a468a0b3290f6a53e4650b7b14bbd7e0fc8f80
ekuinox <depkey@me.com>
2020-05-05 16:59:52 +0900

    セッションをMapにした

--- backend/src/routes/auth.rs
+++ backend/src/routes/auth.rs
@@ -3,13 +3,16 @@ use serde::{Serialize, Deserialize};
 use oauth2::PkceCodeVerifier;
 use crate::github::*;
 use crate::types::Session;
+use json_dotpath::DotPaths;
+
+const PKCE_VERIFIER_PATH: &'static str = "PKCE_VERIFIER";
 
 #[get("/request_token")]
 pub fn request_token(session: Session) -> Redirect {
     let (authorize_url, _, _, pkce_verifier) = get_authorize_url();
 
-    session.tap(|v| {
-        *v = pkce_verifier.secret().clone();
+    let _ = session.tap(|v| {
+        v.dot_set(PKCE_VERIFIER_PATH, pkce_verifier.secret().clone())
     });
 
     Redirect::to(authorize_url.into_string())
@@ -22,22 +25,27 @@ struct GetAuthenticatedUserResponse {
 
 #[get("/auth?<code>&<state>")]
 pub fn authorize(code: String, state: String, session: Session) -> Redirect {
-    let access_token = exchange_code_to_access_token(code, session.tap(|data| {
-        PkceCodeVerifier::new((*data).clone())
-    }));
+    let pkce_verifier_opt = session.tap(|data| {
+        let result: Option<Option<serde_json::Value>> = data.dot_get(PKCE_VERIFIER_PATH).ok();
+        result.flatten().map(|verifier| { PkceCodeVerifier::new(verifier.as_str().unwrap().to_string()) })
+    });
 
     Redirect::to({
-        if let Some(token) = access_token {
-            let username = create_api_client(&token.secret())
-                .get("https://api.github.com/user")
-                .send().map(|response| {
-                    if let Ok(authenticated_user_response) = response.json::<GetAuthenticatedUserResponse>() { // when status 200
-                        authenticated_user_response.login
-                    } else {
-                        "".to_string()
-                    }
-                }).unwrap_or("".to_string());
-            format!("/?token={}&username={}", token.secret(), username)
+        if let Some(pkce_verifier) = pkce_verifier_opt {
+            if let Some(token) = exchange_code_to_access_token(code, pkce_verifier) {
+                let username = create_api_client(&token.secret())
+                    .get("https://api.github.com/user")
+                    .send().map(|response| {
+                        if let Ok(authenticated_user_response) = response.json::<GetAuthenticatedUserResponse>() { // when status 200
+                            authenticated_user_response.login
+                        } else {
+                            "".to_string()
+                        }
+                    }).unwrap_or("".to_string());
+                format!("/?token={}&username={}", token.secret(), username)
+            } else {
+                format!("/")
+            }
         } else {
             format!("/")
         }
--- backend/src/types.rs
+++ backend/src/types.rs
@@ -1,1 +1,3 @@
-pub type Session<'a> = rocket_session::Session<'a, String>;
+use serde_json::Value;
+
+pub type Session<'a> = rocket_session::Session<'a, serde_json::Map<String, Value>>;
