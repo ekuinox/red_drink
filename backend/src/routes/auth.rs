b5086aaace5f4a214bf02ef357a6f7232d103502
ekuinox <depkey@me.com>
2020-05-06 04:18:03 +0900

    セッションにトークンを保存してアーするように（セキュリティ的に何が正しいのかわからん

--- backend/src/routes/auth.rs
+++ backend/src/routes/auth.rs
@@ -4,8 +4,10 @@ use oauth2::PkceCodeVerifier;
 use crate::github::*;
 use crate::types::Session;
 use json_dotpath::DotPaths;
+use rocket_contrib::json::Json;
 
 const PKCE_VERIFIER_PATH: &'static str = "PKCE_VERIFIER";
+pub const GITHUB_ACCESS_TOKEN_PATH: &'static str = "GITHUB_ACCESS_TOKEN";
 
 #[get("/request_token")]
 pub fn request_token(session: Session) -> Redirect {
@@ -20,26 +22,43 @@ pub fn request_token(session: Session) -> Redirect {
 
 #[derive(Debug, Serialize, Deserialize)]
 struct GetAuthenticatedUserResponse {
-    login: String
+    login: String,
+    id: u64
 }
 
 #[get("/auth?<code>&<state>")]
 pub fn authorize(code: String, state: String, session: Session) -> Redirect {
-    Redirect::to({
-        session.tap(|data| {
-            data.dot_get(PKCE_VERIFIER_PATH).ok().flatten().map(|verifier: serde_json::Value| { PkceCodeVerifier::new(verifier.as_str().unwrap().to_string()) })
-        }).map(|pkce_verifier| { exchange_code_to_access_token(code, pkce_verifier) })
-        .flatten()
-        .map(|token| {
-            create_api_client(&token.secret())
-                .get("https://api.github.com/user")
-                .send().map(|response| {
-                    response.json::<GetAuthenticatedUserResponse>().ok().map(|authenticated_user_response| { (token, authenticated_user_response.login) })
-                }).ok().flatten()
-        })
-        .flatten()
-        .map_or(format!("/"), |(token, username)| {
-            format!("/?token={}&username={}", token.secret(), username)
+    if let Some((token, authenticate_user_response)) = session.tap(|data| {
+        data.dot_get(PKCE_VERIFIER_PATH).ok().flatten().map(|verifier: serde_json::Value| { PkceCodeVerifier::new(verifier.as_str().unwrap().to_string()) })
+    }).map(|pkce_verifier| { exchange_code_to_access_token(code, pkce_verifier) })
+    .flatten()
+    .map(|token| {
+        create_api_client(&token.secret())
+            .get("https://api.github.com/user")
+            .send().map(|response| {
+                response.json::<GetAuthenticatedUserResponse>().ok().map(|authenticated_user_response| { (token, authenticated_user_response) })
+            }).ok().flatten()
+    })
+    .flatten() {
+        let _ = session.tap(|data| {
+            data.dot_set(GITHUB_ACCESS_TOKEN_PATH, token)
+        });
+    }
+    Redirect::to(format!("/"))
+}
+
+#[derive(Serialize)]
+pub struct GetTokenResponse {
+    pub token: Option<String>
+}
+
+#[get("/token")]
+pub fn get_token(session: Session) -> Json<GetTokenResponse> {
+    Json(GetTokenResponse {
+        token: session.tap(|data| {
+            data.dot_get(GITHUB_ACCESS_TOKEN_PATH).ok().flatten().map(|value: serde_json::Value| {
+                value.as_str().map(|token| { token.to_string() })
+            }).flatten()
         })
     })
 }
--- frontend/src/Index.tsx
+++ frontend/src/Index.tsx
@@ -3,22 +3,13 @@ import { render } from 'react-dom'
 import * as qs from 'qs'
 import { App } from './App'
 
-interface Window_ extends Window {
-    token?: string
-}
-
-declare const window: Window_
-
-if (window.token == null) {
-    const token: string | undefined = qs.parse(window.location.search.replace(/^\?/, '')).token
-    if (token == null) {
+fetch('/token').then(r => r.json()).then(data => {
+    if (data == null || data.token == null) {
         window.location.replace('/request_token')
     } else {
-        window.token = token
+        render(
+            <App token={data.token} />,
+            document.getElementById('root')
+        )
     }
-}
-
-render(
-    <App token={window.token} />,
-    document.getElementById('root')
-)
+})
