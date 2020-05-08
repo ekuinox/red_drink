74b0ce7d09e370befc308029f2cb0dfd09cde02e
ekuinox <depkey@me.com>
2020-05-08 20:28:27 +0900

    avatarが出るように

--- frontend/src/components/top_bar.tsx
+++ frontend/src/components/top_bar.tsx
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Button from '@material-ui/core/Button'
-import { Link } from '@material-ui/core'
+import Avatar from '@material-ui/core/Avatar'
 
 interface GitHubUser {
     name: string
@@ -15,10 +15,30 @@ const useStyles = makeStyles((theme) => ({
     root: { flexGrow: 1 },
     menuButton: { marginRight: theme.spacing(2) },
     title: { flexGrow: 1 }
-}));
+}))
+
+const GitHubAvatar = ({user}: { user?: GitHubUser }) => {
+    if (user == null) return <div></div>
+    return (
+        <div>
+            { user.login }
+            <Avatar alt={user.name} src={user.avatar_url} />
+        </div>
+    )
+}
 
 export const TopBar = ({token}: { token?: string}) => {
     const classes = useStyles()
+    const [user, setUser] = React.useState<GitHubUser | null>()
+    React.useEffect(() => {
+        if (token != null) {
+            fetch('https://api.github.com/user', {
+                method: 'GET',
+                headers: { Authorization: `token ${token}` }
+            }).then(r => r.json()).then(setUser)
+        }
+    }, [ token ])
+    
     return (
         <div className={classes.root}>
             <AppBar position="static">
@@ -26,6 +46,7 @@ export const TopBar = ({token}: { token?: string}) => {
                     <Button variant="contained" color="secondary" href={ token == null ? "/login" : "/logout" }>
                         { token == null ? "Login" : "Logout" }
                     </Button>
+                    <GitHubAvatar user={user} />
                 </Toolbar>
             </AppBar>
         </div>
