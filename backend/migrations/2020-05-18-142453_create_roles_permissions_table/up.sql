eb47c2fc529f3d575245410d7f62192b65459076
ekuinox <depkey@me.com>
2020-05-19 01:15:17 +0900

    permissionとroleの関連付け

---
+++ backend/migrations/2020-05-18-142453_create_roles_permissions_table/up.sql
@@ -0,0 +1,8 @@
+create table roles_permissions (
+    role_id integer not null,
+    permission_path varchar not null,
+    created_at timestamp not null,
+    primary key(role_id, permission_path),
+    foreign key (role_id) references roles (id),
+    foreign key (permission_path) references permissions (path)
+)
--- backend/src/schema.rs
+++ backend/src/schema.rs
@@ -15,7 +15,19 @@ table! {
     }
 }
 
+table! {
+    roles_permissions (role_id, permission_path) {
+        role_id -> Int4,
+        permission_path -> Varchar,
+        created_at -> Timestamp,
+    }
+}
+
+joinable!(roles_permissions -> permissions (permission_path));
+joinable!(roles_permissions -> roles (role_id));
+
 allow_tables_to_appear_in_same_query!(
     permissions,
     roles,
+    roles_permissions,
 );
