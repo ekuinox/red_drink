fed20b86914604daa328cc2fc8d8ef8dc8441634
ekuinox <depkey@me.com>
2020-05-18 22:58:30 +0900

    roleテーブルついか

---
+++ backend/migrations/2020-05-18-133510_create_roles_table/up.sql
@@ -0,0 +1,5 @@
+create table roles (
+    id integer primary key,
+    name varchar not null unique,
+    created_at timestamp not null
+)
--- backend/src/schema.rs
+++ backend/src/schema.rs
@@ -6,3 +6,16 @@ table! {
         created_at -> Timestamp,
     }
 }
+
+table! {
+    roles (id) {
+        id -> Int4,
+        name -> Varchar,
+        created_at -> Timestamp,
+    }
+}
+
+allow_tables_to_appear_in_same_query!(
+    permissions,
+    roles,
+);
