c7089db87e722750f7794b0665695bb7d01a6193
ekuinox <depkey@me.com>
2020-06-07 21:01:03 +0900

    全部matchでつないだ

--- backend/src/bin/create_user.rs
+++ backend/src/bin/create_user.rs
@@ -9,16 +9,14 @@ use std::env;
  * GitHubのアカウントが存在するかの確認を行わない。
  */
 fn main() {
-    if let Some(id) = env::args().collect::<Vec<String>>().get(1).and_then(|id| { id.parse::<i32>().ok() }) {
-        let message = match db::connect().get() {
-            Ok(connection) => match User::create_with_github_id(id, &connection) {
-                Some(user) => format!("github_id: {}, user_id: {}で作成しました。", id, user.id),
+    println!("{}", match env::args().collect::<Vec<String>>().get(1).and_then(|id| { id.parse::<i32>().ok() }) {
+        Some(github_id) => match db::connect().get() {
+            Ok(connection) => match User::create_with_github_id(github_id, &connection) {
+                Some(user) => format!("github_id: {}, user_id: {}で作成しました。", github_id, user.id),
                 None => format!("作成に失敗しました。")
             },
             Err(_) => format!("DBへの接続に失敗しました。")
-        };
-
-        println!("{}", message);
-    }
-    println!("GitHubのユーザIDを指定してください");
+        },
+        None => format!("GitHubのユーザIDを指定してください")
+    })
 }
