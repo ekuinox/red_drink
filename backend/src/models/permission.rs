bfce91429a92efc1e8ae807288525ade03d4b4c8
ekuinox <depkey@me.com>
2020-06-16 23:45:26 +0900

    permissionを新しく作れるように

--- backend/src/models/permission.rs
+++ backend/src/models/permission.rs
@@ -10,6 +10,34 @@ use crate::schema::{permissions};
 pub struct Permission {
     path: String,
     name: String,
-    description: String,
+    description: Option<String>,
     created_at: NaiveDateTime,
+}
+
+impl Permission {
+    pub fn find(path: String, connection: &DBConnection) -> Option<Permission> {
+        permissions::table.find(path).get_result::<Permission>(connection).ok()
+    }
+}
+
+#[table_name = "permissions"]
+#[derive(AsChangeset, Serialize, Deserialize, Insertable, Queryable, PartialEq, Debug)]
+#[primary_key(path)]
+pub struct PermissionInsertable {
+    path: String,
+    name: String,
+    description: Option<String>
+}
+
+impl PermissionInsertable {
+    pub fn new(path: String, name: String, description: Option<String>) -> PermissionInsertable {
+        PermissionInsertable {
+            path, name, description
+        }
+    }
+    
+    pub fn create(self, connection: &DBConnection) -> Option<Permission> {
+        let _ = diesel::insert_into(permissions::table).values(&self).execute(connection);
+        Permission::find(self.path, connection)
+    }
 }
