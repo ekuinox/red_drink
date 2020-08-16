9b569de5d4b6c2179557da5a1c1f41b52a3aa132
ekuinox <depkey@me.com>
2020-08-16 20:13:57 +0900

    "foo.bar.baz" -> ["*", "foo.*", "foo.bar.*", "foo.bar.baz"] 途中

--- backend/src/models/permission.rs
+++ backend/src/models/permission.rs
@@ -14,6 +14,29 @@ pub struct Permission {
     created_at: NaiveDateTime,
 }
 
+fn get_parent_paths(path: &String) -> Vec<String> {
+    let splited = path.split(".").filter(|node| *node != "*");
+    let mut result = splited.fold(vec![String::from("*")], |accumrator, current| {
+        let last = accumrator.last().unwrap().clone().to_string();
+        let last_splited: Vec<&str> = last.split(".").collect();
+        // [*] || ["foo.*"]
+        let mut last_splited = last_splited.iter().map(|str| str.to_string()).collect::<Vec<String>>();
+        // [*, "foo.*"] || [*, "foo.*", "foo.bar.*"]
+        last_splited.insert(last_splited.len() - 1, String::from(current));
+        [&accumrator[..], &vec![last_splited.join(".")][..]].concat()
+    });
+    result.push(path.clone());
+    result
+}
+
+#[test]
+fn test_get_parent_paths() {
+    let path = "foo.bar.baz".to_string();
+    let paths = get_parent_paths(&path);
+    println!("{:?}", paths);
+    println!("{:?}", get_parent_paths(&"foo.xxx.*".to_string()));
+}
+
 impl Permission {
     pub fn find(path: String, connection: &DBConnection) -> Option<Permission> {
         permissions::table.find(path).get_result::<Permission>(connection).ok()
@@ -23,9 +46,8 @@ impl Permission {
      * Permission配列に欲しいPermissionが含まれているか
      */
     pub fn has_permission(permissions: &Vec<Self>, required: String) -> bool {
-        permissions.into_iter().find(|permission| {
-            permission.path == required
-        }).is_some()
+        let paths = get_parent_paths(&required);
+        true
     }
 }
 
