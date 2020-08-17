89b4b8b042c20c9d65539f4d027d3b16060e3e15
ekuinox <depkey@me.com>
2020-08-18 00:30:27 +0900

    とりあえず String で作った

--- backend/src/models/permission.rs
+++ backend/src/models/permission.rs
@@ -14,27 +14,41 @@ pub struct Permission {
     created_at: NaiveDateTime,
 }
 
+/**
+ * foo.bar.bazのようなパスから[*, foo.*, foo.bar.*, foo.bar.baz]なパスの配列を求める
+ */
 fn get_parent_paths(path: &String) -> Vec<String> {
-    let splited = path.split(".").filter(|node| *node != "*");
-    let mut result = splited.fold(vec![String::from("*")], |accumrator, current| {
+    let splited = {
+        let mut splited = path.split(".").collect::<Vec<&str>>();
+        splited.pop();
+        splited
+    };
+    let splited = splited;
+    let mut result = splited.into_iter().fold(vec![String::from("*")], |accumrator, current| {
         let last = accumrator.last().unwrap().clone().to_string();
         let last_splited: Vec<&str> = last.split(".").collect();
         // [*] || ["foo.*"]
-        let mut last_splited = last_splited.iter().map(|str| str.to_string()).collect::<Vec<String>>();
+        let mut last_splited = last_splited.iter().map
+        (|str| str.to_string()).collect::<Vec<String>>();
         // [*, "foo.*"] || [*, "foo.*", "foo.bar.*"]
         last_splited.insert(last_splited.len() - 1, String::from(current));
         [&accumrator[..], &vec![last_splited.join(".")][..]].concat()
     });
     result.push(path.clone());
-    result
+    result.into_iter().fold(Vec::<String>::new(), |mut accumurator, current| {
+        if !accumurator.contains(&current) {
+            accumurator.push(current);
+        }
+        accumurator
+    })
 }
 
 #[test]
 fn test_get_parent_paths() {
-    let path = "foo.bar.baz".to_string();
-    let paths = get_parent_paths(&path);
-    println!("{:?}", paths);
-    println!("{:?}", get_parent_paths(&"foo.xxx.*".to_string()));
+    let to_string_vec = |v: Vec<&str>| v.into_iter().map(|str| str.to_string()).collect::<Vec<String>>();
+    assert_eq!(get_parent_paths(&"foo.bar.baz".to_string()), to_string_vec(vec!["*", "foo.*", "foo.bar.*", "foo.bar.baz"]));
+    assert_eq!(get_parent_paths(&"foo.*".to_string()), to_string_vec(vec!["*", "foo.*"]));
+    assert_eq!(get_parent_paths(&"*".to_string()), to_string_vec(vec!["*"]));
 }
 
 impl Permission {
