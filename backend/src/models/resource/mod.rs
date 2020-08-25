eba12f393a4acfd78a2f0e7c13373c1efd74bbb9
ekuinox <depkey@me.com>
2020-08-25 22:17:54 +0900

    reorder

--- backend/src/models/resource/mod.rs
+++ backend/src/models/resource/mod.rs
@@ -1,8 +1,10 @@
 use chrono::NaiveDateTime;
 use crate::schema::resources;
 
+mod resource_impl;
+
 #[table_name = "resources"]
-#[derive(AsChangeset, Serialize, Deserialize, Insertable, Identifiable, Associations, Queryable, PartialEq, Clone, Debug)]
+#[derive(Serialize, Deserialize, AsChangeset, Insertable, Identifiable, Associations, Queryable, PartialEq, Clone, Debug)]
 #[primary_key(id)]
 pub struct Resource {
     id: String,
