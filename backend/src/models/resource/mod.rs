636e4beb662597e4b4544147178a33d4d8bd9c49
ekuinox <depkey@me.com>
2020-08-25 21:55:18 +0900

    remove copy derive

--- backend/src/models/resource/mod.rs
+++ backend/src/models/resource/mod.rs
@@ -2,7 +2,7 @@ use chrono::NaiveDateTime;
 use crate::schema::resources;
 
 #[table_name = "resources"]
-#[derive(AsChangeset, Serialize, Deserialize, Insertable, Identifiable, Associations, Queryable, PartialEq, Clone, Copy, Debug)]
+#[derive(AsChangeset, Serialize, Deserialize, Insertable, Identifiable, Associations, Queryable, PartialEq, Clone, Debug)]
 #[primary_key(id)]
 pub struct Resource {
     id: String,
