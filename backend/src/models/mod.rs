64933edcc8261ad07c558950492112148450ec50
ekuinox <depkey@me.com>
2020-08-26 23:53:48 +0900

    add re-exports

--- backend/src/models/mod.rs
+++ backend/src/models/mod.rs
@@ -6,4 +6,9 @@ pub mod permission;
 pub mod users_roles;
 pub mod traits;
 
+// re-exports
 pub use resource::*;
+pub use github_user::*;
+pub use permission::*;
+pub use role::*;
+pub use users_roles::*;
