4d48ab7045be83d25ef7ebbb4f0b5cfc30a99dfb
ekuinox <depkey@me.com>
2020-08-27 23:56:43 +0900

    #6 とりあえずget_permissionsを...（未テスト）

--- backend/src/models/accessible/accessible_impl.rs
+++ backend/src/models/accessible/accessible_impl.rs
@@ -1,11 +1,29 @@
 use diesel;
 use diesel::prelude::*;
+use diesel::associations::HasTable;
+use diesel::result::Error as DieselError;
 use crate::db::DBConnection;
-use crate::models::{traits::*, Accessible};
+use crate::models::{traits::*, Accessible, Permission, Role};
+use crate::schema::accessibles;
 
 impl Find<Accessible, (i32, String, String)> for Accessible {
     fn find(id: (i32, String, String), conn: &DBConnection) -> Option<Accessible> {
-        use diesel::associations::HasTable;
         Self::table().find(id).first::<Accessible>(conn).ok()
+    }
+}
+
+impl Accessible {
+    /// roleが持つ指定したリソースに対しての権限を取得する
+    fn get_permissions(role_id: i32, resource_id: String, conn: &DBConnection) -> Result<Vec<Permission>, DieselError> {
+        Self::table()
+            .inner_join(Permission::table())
+            .filter(accessibles::role_id.eq(role_id))
+            .filter(accessibles::resource_id.eq(resource_id))
+            .get_results::<(Self, Permission)>(conn)
+            .map(|results| results.into_iter().unzip::<_, _, Vec<_>, Vec<Permission>>().1)
+    }
+    /// rootリソースに対してget_permissionsを行う
+    fn get_permissions_for_root(role_id: i32, conn: &DBConnection) -> Result<Vec<Permission>, DieselError> {
+        Self::get_permissions(role_id, "*".to_string(), conn)
     }
 }
