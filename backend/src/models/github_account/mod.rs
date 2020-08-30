e79132ca02b00a1210a2a9ebc855d78dc91c2374
ekuinox <depkey@me.com>
2020-08-30 19:05:45 +0900

    add Identifiable

--- backend/src/models/github_account/mod.rs
+++ backend/src/models/github_account/mod.rs
@@ -11,7 +11,7 @@ mod create_impl;
  * GitHubアカウントとUserを紐付ける
  */
 #[table_name = "github_accounts"]
-#[derive(AsChangeset, Serialize, Deserialize, Insertable, Queryable, Associations, PartialEq, Debug)]
+#[derive(AsChangeset, Serialize, Deserialize, Identifiable, Insertable, Queryable, Associations, PartialEq, Debug)]
 #[belongs_to(User, foreign_key = "user_id")]
 #[primary_key(github_id)]
 pub struct GitHubAccount {
