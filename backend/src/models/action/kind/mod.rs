337a4743a94081eb8f1b9c91e9412a70b819953c
ekuinox <depkey@me.com>
2020-09-03 23:55:27 +0900

    #4 FromSqlの実装

--- backend/src/models/action/kind/mod.rs
+++ backend/src/models/action/kind/mod.rs
@@ -1,22 +1,35 @@
 use diesel::sql_types::*;
-use diesel::serialize::*;
+use diesel::serialize;
 use diesel::deserialize;
 use diesel::backend::Backend;
 use std::io::Write;
 
-#[derive(Serialize, Deserialize, AsExpression, Debug, Clone)]
+#[derive(FromSqlRow, AsExpression, Serialize, Deserialize, AsExpression, PartialEq, Debug, Clone)]
 pub struct RunCommand {
     cmd: String
 }
 
-impl <DB> ToSql<Json, DB> for RunCommand
+impl <DB> serialize::ToSql<Json, DB> for RunCommand
 where
     DB: Backend,
-    String: ToSql<Json, DB>,
+    String: serialize::ToSql<Json, DB>,
 {
-    fn to_sql<W: Write>(&self, out: &mut Output<W, DB>) -> Result {
+    fn to_sql<W: Write>(&self, out: &mut serialize::Output<W, DB>) -> serialize::Result {
         match serde_json::to_string(self).map(|s| s.to_sql(out)) {
             Ok(a) => a,
+            Err(err) => Err(Box::new(err))
+        }
+    }
+}
+
+impl <DB> deserialize::FromSql<Json, DB> for RunCommand
+where
+    DB: Backend,
+    String: deserialize::FromSql<Json, DB>,
+{
+    fn from_sql(bytes: Option<&DB::RawValue>) -> deserialize::Result<Self> {
+        match serde_json::from_str::<Self>(String::from_sql(bytes)?.to_string().as_str()) {
+            Ok(a) => Ok(a),
             Err(err) => Err(Box::new(err))
         }
     }
