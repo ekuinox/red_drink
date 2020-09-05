40cad9b0df24324b64c8e391c650cfa5e3f3453a
ekuinox <depkey@me.com>
2020-09-06 04:57:34 +0900

    #4 added test

--- backend/src/models/action/descriptor/eval.rs
+++ backend/src/models/action/descriptor/eval.rs
@@ -34,17 +34,39 @@ impl AsKind for EvalDescriptor {
 
 impl Executable<std::process::Output> for EvalDescriptor {
     fn execute(&self, ctx: ExecutableContext) -> Result<std::process::Output, ExecutableError> {
-        let is_denined = self.required_permissons.iter()
-            .find(|required| !ctx.executor.has_permission(required.to_owned().to_owned(), None, ctx.conn))
-            .is_some();
-        if is_denined {
-            Err(ExecutableError::AccessDenied)
-        } else {
+        let is_allowed = self.required_permissons.is_empty() || !self.required_permissons.iter()
+            .any(|required| !ctx.executor.has_permission(required.to_owned().to_owned(), None, ctx.conn));
+        if is_allowed {
             Command::new(self.shell.clone())
-                .arg("-c")
-                .arg(self.command.clone())
-                .output()
-                .map_err(|err| ExecutableError::IOError(err))
+            .arg("-c")
+            .arg(self.command.clone())
+            .output()
+            .map_err(|err| ExecutableError::IOError(err))
+        } else {
+            Err(ExecutableError::AccessDenied)
         }
     }
+}
+
+#[test]
+fn test_eval_command() {
+    use diesel::prelude::*;
+    use crate::models::User;
+    use crate::db::connect;
+
+    let conn = connect().get().expect("could not established connection");
+    conn.test_transaction::<_, diesel::result::Error, _>(|| {
+        let user = User::create_with_github_id(0, &conn)?;
+        let r = user.add_role(0, &conn);
+        assert!(r);
+
+        let eval = EvalDescriptor {
+            command: "echo hello".to_string(),
+            required_permissons: vec!["*".to_owned()],
+            ..Default::default()
+        };
+        assert!(eval.execute(ExecutableContext { executor: &user, conn: &conn }).is_ok());
+
+        Ok(())
+    });
 }
