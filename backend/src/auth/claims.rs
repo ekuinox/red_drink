bc8dae0f1104e6ffbecddc944a8286adc2ae39ec
ekuinox <depkey@me.com>
2020-09-13 16:02:57 +0900

    impl as_user

--- backend/src/auth/claims.rs
+++ backend/src/auth/claims.rs
@@ -1,4 +1,7 @@
 use chrono::{Utc, Duration};
+use crate::models::user::{User, AsUser};
+use crate::db::DBConnection;
+use crate::types::DieselError;
 
 /// 有効期間
 pub const VALIDITY_DAYS: i64 = 7;
@@ -15,5 +18,11 @@ impl Claims {
             uid: id,
             exp: (Utc::now() + Duration::days(VALIDITY_DAYS)).timestamp()
         }
+    }
+}
+
+impl AsUser<DieselError> for Claims {
+    fn as_user(self, conn: &DBConnection) -> Result<User, DieselError> {
+        self.uid.as_user(conn)
     }
 }
