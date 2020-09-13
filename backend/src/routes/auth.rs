b75835ba97890e34419f40ed969354a17dd8e01b
ekuinox <depkey@me.com>
2020-09-13 21:01:42 +0900

    set cookie

--- backend/src/routes/auth.rs
+++ backend/src/routes/auth.rs
@@ -2,6 +2,8 @@ use rocket::response::Redirect;
 use serde::{Serialize, Deserialize};
 use oauth2::PkceCodeVerifier;
 use json_dotpath::DotPaths;
+use rocket::http::Cookies;
+use crate::auth::Claims;
 use crate::github::*;
 use crate::types::Session;
 use crate::db::Connection;
@@ -34,25 +36,26 @@ struct GetAuthenticatedUserResponse {
 }
 
 #[get("/auth?<code>")]
-pub fn authorize(code: String, session: Session, connection: Connection) -> Redirect {
-    if let Some((token, user)) = session.tap(|data| {
-        data.dot_get(PKCE_VERIFIER_PATH).ok().flatten().map(|verifier: serde_json::Value| { PkceCodeVerifier::new(verifier.as_str().unwrap().to_string()) })
-    }).map(|pkce_verifier| { exchange_code_to_access_token(code, pkce_verifier) })
-    .flatten()
-    .map(|token| {
-        create_api_client(&token.secret())
-            .get("https://api.github.com/user")
-            .send().map(|response| {
-                response.json::<GetAuthenticatedUserResponse>().ok().map(|authenticated_user_response| { (token, authenticated_user_response) })
-            }).ok().flatten()
-    })
-    .flatten() {
-        let _ = session.tap(|data| {
-            data.dot_set(GITHUB_ACCESS_TOKEN_PATH, token)
-        });
-        // アカウントが存在するか確認し、存在しなければ作成する
-        let _ = User::find_by_github_id(user.id as i32, &connection);
-    }
-    
-    Redirect::to(format!("/"))
+pub fn authorize(code: String, session: Session, mut cookies: Cookies, connection: Connection) -> Redirect {
+    if let Some(cookie) = session.tap(|data| data.dot_get(PKCE_VERIFIER_PATH).ok().flatten())
+        .and_then(|verifier: serde_json::Value| exchange_code_to_access_token(code, PkceCodeVerifier::new(verifier.to_string())))
+        .and_then(|token| {
+            create_api_client(&token.secret())
+                .get("https://api.github.com/user")
+                .send().and_then(|response| {
+                    response.json::<GetAuthenticatedUserResponse>()
+                        .map(|authenticated_user_response| { (token, authenticated_user_response) })
+                }).ok()
+        })
+        .and_then(|(_, user)| {
+            User::find_or_new_by_github_id(user.id as i32, &connection).ok()
+        })
+        .and_then(|user| {
+            Claims::new(user.id).to_token()
+                .map(|token| token.to_cookie())
+                .ok()
+        }) {
+            cookies.add_private(cookie);
+        }
+    Redirect::to("/")
 }
