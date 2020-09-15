12c91f0c393eb8b9105db1460fdb4171fa938282
ekuinox <depkey@me.com>
2020-09-15 22:54:12 +0900

    lint fix

--- frontend/webpack.config.ts
+++ frontend/webpack.config.ts
@@ -1,35 +1,40 @@
 import * as HtmlWebpackPlugin from 'html-webpack-plugin';
 import { Configuration } from 'webpack';
 
-const isMode = (mode?: string): mode is 'development' | 'production' | 'none' => ['development', 'production', 'none'].includes(mode ?? '');
+const isMode = (mode?: string): mode is 'development' | 'production' | 'none' =>
+  ['development', 'production', 'none'].includes(mode ?? '');
 
-const env = isMode(process.env.WEBPACK_ENV) ? process.env.WEBPACK_ENV : 'production';
+const env = isMode(process.env.WEBPACK_ENV)
+  ? process.env.WEBPACK_ENV
+  : 'production';
 
 const conf: Configuration = {
-    mode: env,
-	devtool: env === 'development' ? "inline-source-map" : undefined,
-	entry: './src/Index.tsx',
-	output: {
-		path: `${__dirname}/dist`,
-		filename: 'bundle.js',
-	},
-	resolve: {
-		extensions: ['.ts', '.tsx', '.js']
-	},
-	module: {
-		rules: [{
-			test: /\.tsx?$/,
-			exclude: /node_modules/,
-			loader: 'babel-loader'
-		}]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			title: "red drink",
-			inject: false,
-			template: "./src/index.html"
-		})
-	]
+  mode: env,
+  devtool: env === 'development' ? 'inline-source-map' : undefined,
+  entry: './src/Index.tsx',
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'red drink',
+      inject: false,
+      template: './src/index.html',
+    }),
+  ],
 };
 
 export default conf;
