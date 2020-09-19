d664da03f7ea7ee766fa72fd390c96f897125db8
ekuinox <depkey@me.com>
2020-09-19 19:26:05 +0900

    いらなさそうなのは消すのだわ

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,14 +1,7 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-extern crate base64;
-extern crate oauth2;
-extern crate rand;
-extern crate http;
-extern crate rocket_contrib;
 #[macro_use] extern crate rocket;
 #[macro_use] extern crate serde;
-extern crate serde_json;
-extern crate red_drink;
 
 mod auth;
 mod routes;
