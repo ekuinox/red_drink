35f1b871662b96e3b9435c9816e9713af6a52acc
ekuinox <depkey@me.com>
2020-09-22 13:43:26 +0900

    失敗時にStatusを返すように

--- red_drink_server/src/routes/api/user/get_user.rs
+++ red_drink_server/src/routes/api/user/get_user.rs
@@ -1,5 +1,7 @@
 use serde::{Serialize, Deserialize};
 use rocket_contrib::json::Json;
+use rocket::response::status::{Custom, NotFound};
+use rocket::http::Status;
 use chrono::NaiveDateTime;
 use crate::db::Connection;
 use crate::auth::Claims;
@@ -14,23 +16,23 @@ pub struct UserResponse {
 }
 
 #[get("/user")]
-pub fn get(claims: Claims, conn: Connection) -> Json<Option<UserResponse>> {
+pub fn get(claims: Claims, conn: Connection) -> Result<Json<UserResponse>, Custom<()>> {
     use crate::models::{User, user::AsUser};
-    Json(claims.as_user(&conn)
+    claims.as_user(&conn)
         .map(|User { id, name, avatar_url, email, created_at }: User| UserResponse {
             id, name, avatar_url, email, created_at
         })
-        .ok()
-    )
+        .map_err(|_| Custom(Status::InternalServerError, ()))
+        .map(|user| Json(user))
 }
 
 #[get("/user/<username>")]
-pub fn get_user_by_username(_claims: Claims, username: String, conn: Connection) -> Json<Option<UserResponse>> {
+pub fn get_user_by_username(_claims: Claims, username: String, conn: Connection) -> Result<Json<UserResponse>, NotFound<()>> {
     use crate::models::{User, traits::Find};
-    Json(User::find(username, &conn)
+    User::find(username, &conn)
         .map(|User { id, name, avatar_url, email, created_at }: User| UserResponse {
             id, name, avatar_url, email, created_at
         })
-        .ok()
-    )
+        .map_err(|_| NotFound(()))
+        .map(|user| Json(user))
 }
