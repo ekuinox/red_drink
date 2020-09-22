9a7b4c77d71150be0e37b17c06e9a28ea4281c64
ekuinox <depkey@me.com>
2020-09-22 14:58:01 +0900

    ユーザ自身のRoleをpermissions込で取得するAPIの追加

--- red_drink_server/src/routes/api/user/mod.rs
+++ red_drink_server/src/routes/api/user/mod.rs
@@ -2,13 +2,15 @@ use rocket::Route;
 use crate::routes::Routes;
 
 mod get_user;
+mod roles;
 
 use self::get_user::*;
+use self::roles::*;
 
 pub(crate) struct UserRoutes;
 
 impl Routes for UserRoutes {
     fn routes() -> Vec<Route> {
-        routes![self::get, self::get_user_by_username]
+        routes![get, get_user_by_username, get_roles]
     }
 }
