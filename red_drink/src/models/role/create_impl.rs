53c5ab52c69625056a82c96365f6b89eccb3a074
ekuinox <depkey@me.com>
2020-10-11 23:58:29 +0900

    create accessible

--- red_drink/src/models/role/create_impl.rs
+++ red_drink/src/models/role/create_impl.rs
@@ -4,29 +4,39 @@ use diesel::result::Error as DieselError;
 use crate::db::DBConnection;
 use crate::models::{traits::*, Role};
 use crate::schema::roles;
+use super::accessible::Accessible;
 
 #[table_name = "roles"]
 #[derive(Serialize, Deserialize, Insertable, Queryable, PartialEq, Debug, Clone)]
 struct RoleBuilder {
-    pub name: String
+    pub name: String,
+    pub accessible: Accessible
 }
 
 impl RoleBuilder {
     fn new(name: String) -> RoleBuilder {
-        RoleBuilder { name }
+        RoleBuilder { name, accessible: Default::default() }
     }
-    
+    fn accessible(self, accessible: Accessible) -> RoleBuilder {
+        RoleBuilder { accessible, ..self }
+    }
     fn save(self, connection: &DBConnection) -> Result<Role, DieselError> {
         diesel::insert_into(roles::table)
             .values(self)
-            .returning((roles::id, roles::name, roles::created_at))
+            .returning((roles::id, roles::name, roles::created_at, roles::accessible))
             .get_result(connection)
-            .map(|(id, name, created_at)| Role { id, name, created_at, accessible: Default::default() })
+            .map(|(id, name, created_at, accessible)| Role { id, name, created_at, accessible })
     }
 }
 
 impl Create<Role, DieselError, String> for Role {
     fn create(name: String, conn: &DBConnection) -> Result<Role, DieselError> {
         RoleBuilder::new(name).save(conn)
+    }
+}
+
+impl Create<Role, DieselError, (String, Accessible)> for Role {
+    fn create((name, accessible): (String, Accessible), conn:  &DBConnection) -> Result<Role, DieselError> {
+        RoleBuilder::new(name).accessible(accessible).save(conn)
     }
 }
