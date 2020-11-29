b5099e08139819b4caf2bc9994af636a785aa435
ekuinox <depkey@me.com>
2020-11-29 18:55:51 +0900

    #17 Role::has_permissionのテストを追加

--- red_drink/src/models/role/role_impl.rs
+++ red_drink/src/models/role/role_impl.rs
@@ -6,6 +6,7 @@ use crate::types::DieselError;
 use crate::models::{Role, traits::*};
 use crate::models::resource_id::ResourceId;
 
+
 impl Find<Role, DieselError, i32> for Role {
     fn find(id: i32, conn: &DBConnection) -> Result<Role, DieselError> {
         roles::table.find(id).first::<Role>(conn)
@@ -23,4 +24,30 @@ impl Role {
     pub fn all(connection: &DBConnection) -> Result<Vec<Role>, DieselError> {
         roles::table.load::<Role>(connection)
     }
+}
+
+#[test]
+fn test_has_permission() {
+    use chrono::Utc;
+    use crate::models::resource_id::ROOT_RESOURCE;
+    use super::{Policy, Permission};
+
+    let p1 = Policy {
+        resources: vec![ROOT_RESOURCE.clone()],
+        permissions: vec![Permission::from("foo.bar.*"), Permission::from("xxx.*")],
+        ..Default::default()
+    };
+    
+    let r1 = Role {
+        id: 0,
+        name: "test role".to_string(),
+        policy: p1,
+        created_at: Utc::now().naive_utc()
+    };
+
+    assert!(r1.has_permission("foo.bar.baz".to_string(), ROOT_RESOURCE.clone()));
+    assert!(r1.has_permission("foo.bar.*".to_string(), ROOT_RESOURCE.clone()));
+    assert!(r1.has_permission("xxx.yyy.zzz".to_string(), ROOT_RESOURCE.clone()));
+    assert!(!r1.has_permission("foo.abc.*".to_string(), ROOT_RESOURCE.clone()));
+    assert!(!r1.has_permission("abc.*".to_string(), ROOT_RESOURCE.clone()));
 }
