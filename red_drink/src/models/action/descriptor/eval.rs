fbd1b61eaad5f5abe984dfaa5bcf05ee2ce8abb5
ekuinox <depkey@me.com>
2020-11-29 23:48:27 +0900

    #17 テストを追加ァ...

--- red_drink/src/models/action/descriptor/eval.rs
+++ red_drink/src/models/action/descriptor/eval.rs
@@ -57,5 +57,47 @@ impl Executable<std::process::Output> for EvalDescriptor {
 
 #[test]
 fn test_eval_command() {
-    // TODO
+    use diesel;
+    use diesel::prelude::*;
+    use crate::models::resource_id::ROOT_RESOURCE;
+    use crate::models::{role::{Policy, Permission, Role}, User, traits::*};
+    use crate::db::connect;
+
+    let p1 = Policy {
+        resources: vec![ROOT_RESOURCE.clone()],
+        permissions: vec![Permission::from("foo.bar.*"), Permission::from("xxx.*")],
+        ..Default::default()
+    };
+    
+    let conn = connect().get().expect("cannnot get connection");
+    conn.test_transaction::<_, diesel::result::Error, _>(|| {
+        let u1 = User::create("test user".to_string(), &conn)?;
+        let r1 = Role::create(("test role".to_string(), p1), &conn)?;
+        u1.add_role(r1.id, &conn);
+
+        let ctx = ExecutableContext {
+            executor: &u1,
+            conn: &conn
+        };
+
+        let desc1 = EvalDescriptor {
+            shell: "bash".to_string(),
+            command: "echo \"hello world\"".to_string(),
+            resources: vec![(ROOT_RESOURCE.clone(), vec!["foo.bar.baz".to_string()])]
+        };
+        assert!(desc1.is_allowed(&ctx));
+
+        let desc2 = EvalDescriptor {
+            shell: "bash".to_string(),
+            command: "echo \"hello world\"".to_string(),
+            resources: vec![(ROOT_RESOURCE.clone(), vec![
+                "foo.bar.baz".to_string(),
+                "foo.bar.*".to_string(),
+                "xxx.yyy.zzz".to_string()
+                ])]
+        };
+        assert!(desc2.is_allowed(&ctx));
+
+        Ok(())
+    });
 }
