52bf8c5b66261fd1f2143d2e30d84d2885712d81
ekuinox <depkey@me.com>
2020-12-01 23:56:27 +0900

    routesに追加

--- red_drink_server/src/routes/api/mod.rs
+++ red_drink_server/src/routes/api/mod.rs
@@ -1,14 +1,19 @@
 use rocket::Route;
 use crate::routes::Routes;
 
+mod action;
 mod user;
 
+use action::ActionRoutes;
 use user::UserRoutes;
 
 pub(crate) struct ApiRoutes;
 
 impl Routes for ApiRoutes {
     fn routes() -> Vec<Route> {
-        [UserRoutes::routes()].concat()
+        [
+            UserRoutes::routes(),
+            ActionRoutes::routes()
+        ].concat()
     }
 }
